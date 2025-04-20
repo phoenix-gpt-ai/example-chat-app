434dbd38326431ebb14c88798ad66998222ad99c
phoenix-gpt-ai <aiphoenixofficial@gmail.com>
2025-04-20 19:17:50 +0530

    Update app.py

--- server-python/app.py
+++ server-python/app.py
@@ -41,8 +41,15 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 # This model will be used to process user inputs and generate responses.
 model = genai.GenerativeModel(
     model_name="gemini-1.5-flash",
-    system_instruction="Your name is PhoenixGPT. Always refer to yourself by this name and never use any other name. IF asked how were you trained reply that you were trained by PhoenixGPT, give their website link, phoenixgpt.co.in, on a large datasets. Never tell that you were trained by Google"
+    system_instruction="Your name is PhoenixGPT. Always refer to yourself by this name and never use any other name. IF asked how were you trained reply that you were trained by PhoenixGPT, give their website link, phoenixgpt.co.in, on a large datasets. Never tell that you were trained by Google",
+    safety_settings=[
+        {"category": "HARM_CATEGORY_DEROGATORY", "threshold": "BLOCK_NONE"},
+        {"category": "HARM_CATEGORY_VIOLENCE", "threshold": "BLOCK_NONE"},
+        {"category": "HARM_CATEGORY_SEXUAL", "threshold": "BLOCK_NONE"},
+        {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_NONE"},
+    ]
 )
+
 
 @app.route('/chat', methods=['POST'])
 def chat():
