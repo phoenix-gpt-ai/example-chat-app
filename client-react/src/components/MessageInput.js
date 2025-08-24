d5a35874c60ddff4c3a0475d34ab3eb0a04b7276
phoenix-gpt-ai <aiphoenixofficial@gmail.com>
2025-08-24 23:55:52 +0530

    Update MessageInput.js

--- client-react/src/components/MessageInput.js
+++ client-react/src/components/MessageInput.js
@@ -19,7 +19,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
-import FileUpload from './FileUpload';
+
 
 /** Submission using the Enter key or button. */
 const MessageInput = ({ 
