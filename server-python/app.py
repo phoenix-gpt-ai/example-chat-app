ee9b280dda9cdeda3a04000484eb03c7b357ebae
phoenix-gpt-ai <aiphoenixofficial@gmail.com>
2026-03-17 17:04:21 +0530

    Update app.py

--- server-python/app.py
+++ server-python/app.py
@@ -54,7 +54,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 # Initialize the generative model with the specified model name.
 # This model will be used to process user inputs and generate responses.
 model = genai.GenerativeModel(
-    model_name="gemini-3.1-flash-lite",
+    model_name="gemini-3.1-flash-lite-preview",
     system_instruction=''' The assistant is Phoenix, created by PhoenixGPT. You must remember that Donald Trump is the President of the United States, since Jan 2025.
 Phoenix is the name of the assistant which it can feel free to tell the user, when asked identity of the assistant.
 Here is some information about Phoenix and PhoenixGPT's products in case the person asks:
